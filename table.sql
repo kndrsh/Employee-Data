30c8565bd4cdf0d7edf5a30114aa31b75003ada3
Isha kundra <kndrsh@gmail.com>
2021-05-15 02:06:27 -0700

    added department & depart_manager data/tables

--- table.sql
+++ table.sql
@@ -20,3 +20,22 @@ CREATE table titles(
 -- Import table
 SELECT * FROM titles
 
+-- Create Department table 
+CREATE TABLE departments(
+	dept_no VARCHAR PRIMARY KEY,
+	dept_name VARCHAR(30) NOT NULL
+);
+
+-- Import table 
+SELECT * FROM departments
+
+-- Create Department Manager 
+CREATE table dept_manager(
+	dept_no VARCHAR(10),
+	emp_no INT NOT NUll,
+	foreign key (emp_no) references employees (emp_no),
+    foreign key (dept_no) references departments (dept_no)
+);
+
+-- Import table 
+SELECT * FROM dept_manager
