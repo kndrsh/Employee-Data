a05f97bf7bc42ea6049869f91b6d9a0d3de2bb44
Isha kundra <kndrsh@gmail.com>
2021-05-15 01:56:58 -0700

    imported data to 2 tables

--- table.sql
+++ table.sql
@@ -1,9 +1,10 @@
 -- Create employees table 
 create table employees( 
-	emp_no INT PRIMARY KEY NOT NULL,
+	emp_no INT PRIMARY KEY NOT NULL ,
+	emp_title VARCHAR (30) NOT NULL,
 	birth_date DATE NOT NULL,
 	first_name VARCHAR(45) NOT NULL,
-	last_name VARCHAR(45) NOT NULL,
+	last_name VARCHAR(45) NOT NULL ,
 	gender VARCHAR(1) NOT NULL,
 	hire_date DATE NOT NULL
 );
@@ -11,4 +12,11 @@ create table employees(
 SELECT * FROM employees
 
 --Create titles table 
+CREATE table titles(
+	title_id VARCHAR(8),
+	title VARCHAR(30)
+);
+
+-- Import table
+SELECT * FROM titles
 
